a9a7ddedc7cebeffe670dded167286d2fc8d2dbb
chz <chz@bingularity.org>
2016-08-06 22:12:42 -0700

    zk/session: better logging, protect futures map

--- zk/session.go
+++ zk/session.go
@@ -1,7 +1,9 @@
 package zk
 
 import (
+	"fmt"
 	"net"
+	"sync"
 
 	"github.com/chzchzchz/zetcd"
 	"github.com/golang/glog"
@@ -18,6 +20,7 @@ type session struct {
 	ctx    context.Context
 	cancel context.CancelFunc
 
+	mu      sync.Mutex
 	futures map[zetcd.Xid]chan zetcd.ZKResponse
 }
 
@@ -81,34 +84,51 @@ func newSession(servers []string, zka zetcd.AuthConn) (*session, error) {
 
 func (s *session) future(xid zetcd.Xid, op interface{}) <-chan zetcd.ZKResponse {
 	ch := make(chan zetcd.ZKResponse, 1)
-	if err := s.zkc.Send(xid, op); err != nil {
-		ch <- zetcd.ZKResponse{Err: err}
+	if s.futures == nil {
+		glog.V(6).Infof("futuresClosed=%+v", op)
+		ch <- zetcd.ZKResponse{Err: fmt.Errorf("closed")}
 		return ch
 	}
+	s.mu.Lock()
 	s.futures[xid] = ch
+	s.mu.Unlock()
+	if err := s.zkc.Send(xid, op); err != nil {
+		ch <- zetcd.ZKResponse{Err: err}
+		s.mu.Lock()
+		delete(s.futures, xid)
+		s.mu.Unlock()
+		return ch
+	}
+	glog.V(6).Infof("waitFutureSendResp=%+v", op)
 	return ch
 }
 
 // recvLoop forwards responses from the real zk server to the zetcd connection.
 func (s *session) recvLoop() {
 	defer func() {
+		s.mu.Lock()
 		for _, ch := range s.futures {
 			close(ch)
 		}
 		s.futures = nil
+		s.mu.Unlock()
 		s.cancel()
 	}()
 	for resp := range s.zkc.Read() {
 		if resp.Err != nil {
-			glog.V(6).Infof("zkresp=Err(%v)", resp.Err)
+			glog.V(6).Infof("zk/zkresp=Err(%v)", resp.Err)
 			return
 		}
-		glog.V(6).Infof("zkresp=(%+v,%+v)", *resp.Hdr, resp.Resp)
+		glog.V(6).Infof("zk/zkresp=(%+v,%+v)", *resp.Hdr, resp.Resp)
+		s.mu.Lock()
 		if ch := s.futures[resp.Hdr.Xid]; ch != nil {
 			ch <- resp
 			delete(s.futures, resp.Hdr.Xid)
+			s.mu.Unlock()
 			continue
 		}
+		s.mu.Unlock()
+
 		// out of band requests (i.e., watches)
 		var r interface{}
 		if resp.Hdr.Err != 0 {
@@ -116,6 +136,8 @@ func (s *session) recvLoop() {
 		} else {
 			r = &resp.Resp
 		}
+
+		glog.V(6).Infof("zk/zkSessOOB=%+v %+v", resp.Hdr, r)
 		s.Send(resp.Hdr.Xid, resp.Hdr.Zxid, r)
 	}
 }
